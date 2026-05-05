afedaee7ac69764c49dfa57360d6cfaa87ca2a45
Tushar Verma <tushar84391@gmail.com>
2026-05-05 22:11:55 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,19 +1,14 @@
 console.log("Animation script loaded");
 const text = "We are online 😎";
+const text2 = "We will be online at 12 PM";
+const text3 = "We are closed for now 😔";
 let index = 0;
 const speed = 100;
 
 const el = document.getElementById("typing-test");
+const dot = document.getElementById("dot");
 
-function type() {
-  if (index < text.length) {
-    el.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(type, speed);
-  } else {
-    setTimeout(fadeOut, 10000); // wait 10 sec
-  }
-}
+
 
 function fadeOut() {
   el.style.transition = "opacity 1s";
@@ -23,7 +18,8 @@ function fadeOut() {
     el.innerHTML = "";
     el.style.opacity = "1";
     index = 0;
-    type();
+    const newMessage = updateStatus();
+    type(newMessage);
   }, 1000);
 }
 function orderNow(product, price) {
@@ -35,5 +31,37 @@ function orderNow(product, price) {
 
   window.open(url, "_blank");
 }
+// Function to update the status message based on the current time
+function updateStatus() {
+  const el = document.getElementById("typing-test");
 
-type();
+  const now = new Date();
+  const hour = now.getHours(); // gets current hour (0–23)
+
+  if (hour < 12) {
+    dot.style.background = "red"; // Show the red dot for offline status
+     return text2;//"We will be online at 12 PM"
+  } 
+  else if (hour >= 12 && hour < 15) {
+    dot.style.background = "limegreen"; // Show the green dot for online status
+    return text; // "We are online 😎"
+  } 
+  else {
+    dot.style.background = "red"; // Show the red dot for offline status
+    return text3; //"We are closed for now 😔"
+  }
+}
+
+
+function type(message) {
+  if (index < message.length) {
+    el.innerHTML += message.charAt(index);
+    index++;
+    setTimeout(() => type(message), speed);
+  } else {
+    setTimeout(fadeOut, 10000); // wait 10 sec
+  }
+}
+const statusMessage = updateStatus();
+type(statusMessage);
+
